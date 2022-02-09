87da38b0effd5e9290ce50733851f2290d3a0911
EgorBYV <egovrublevskij@gmail.com>
2022-02-09 16:41:48 +0300

    Добавлены NavLink и стили по подсветке активных ссылок.(Правда как-то сложно и не гибко, на основе комментариев к 20 уроку

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,27 @@
 import classes from'./Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className={classes.Navbar}>
-      <div className={classes.item}>
-        <a href="/profile">Profile</a>
+      <div>
+        <NavLink to="/profile" className={navData => navData.isActive ? classes.active : classes.item}>Profile</NavLink>
       </div>
-      <div className={classes.item}>
-        <a href="/dialogs">Messages</a>
+      <div>
+        <NavLink to="/dialogs" className={navData => navData.isActive ? classes.active : classes.item}>Messages</NavLink>
       </div>
-      <div className={classes.item}>
-        <a href="/news">News</a>
+      <div>
+        <NavLink to="/news" className={navData => navData.isActive ? classes.active : classes.item}>News</NavLink>
       </div>
-      <div className={classes.item}>
-        <a href="/contacts">Contacts</a>
+      <div>
+        <NavLink to="/contacts" className={navData => navData.isActive ? classes.active : 0}>Contacts</NavLink>
       </div>
-      <div className={classes.item}>
-        <a href="/music">Music</a>
+      <div>
+        <NavLink to="/music" className={navData => navData.isActive ? classes.active : classes.item}>Music</NavLink>
       </div>
-      <div className={classes.item}>
-        <a href="/settings">Settings</a>
+      <div>
+        <NavLink to="/settings" className={navData => navData.isActive ? classes.active : classes.item}>Settings</NavLink>
       </div>
-
     </nav>);
 };
 export default Navbar;
