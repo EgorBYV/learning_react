0a7ee0f8134c7d938c7211bb5d0e02ec4a26fefd
EgorBYV <egovrublevskij@gmail.com>
2022-02-09 23:51:48 +0300

    Создание массиво данных в переменной и интеграция их в Dialog и Post

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,10 +2,29 @@ import classes from './Dialogs.module.css'
 import Dialog from './Dialog/Dialog'
 import Message from './Message/Message'
 
-const Dialogs = () => {
+const Dialogs = (props) => {
+
+    let dialogsData = [
+        {id: 6, name: 'Alberth'},
+        {id: 7, name: 'Victoria'},
+        {id: 8, name: 'Romualda'},
+        {id: 9, name: 'Germiona'},
+        {id: 10, name: 'Antoniy'},
+    ]
+
+    let messagesData = [
+        {id: 4, text: 'You'},
+        {id: 5, text: 'Kick'},
+        {id: 6, text: 'My name is Barry'},
+        {id: 7, text: 'JavaScript is my favorite language.'},
+        {id: 8, text: "Swift is my wife's favorite language."},
+    ]
+
     return (
         <div className={classes.Dialogs}>
             <div className={classes.dialogs_items}>
+            <Dialog name={dialogsData[0].name} id={dialogsData[0].id} />
+            <Dialog name={dialogsData[1].name} id={dialogsData[1].id} />
                 <Dialog name="Egor" id="1" />
                 <Dialog name="Vasia" id="2" />
                 <Dialog name="Gerald" id="3" />
@@ -13,6 +32,11 @@ const Dialogs = () => {
                 <Dialog name="Inokentiy" id="5" />
             </div>
             <div className={classes.messages}>
+                <Message text={messagesData[0].text}/>
+                <Message text={messagesData[1].text}/>
+                <Message text={messagesData[2].text}/>
+                <Message text={messagesData[3].text}/>
+                <Message text={messagesData[4].text}/>
                 <Message text="Hi" />
                 <Message text="How is your video-course?" />
                 <Message text="Fine, thank you!" />
--- src/components/Profile/My_posts/My_posts.jsx
+++ src/components/Profile/My_posts/My_posts.jsx
@@ -2,6 +2,16 @@ import classes from './My_posts.module.css'
 import Post from './Post/Post';
 
 const My_posts = () => {
+
+  let postsData = [
+    {id: 1, text: 'Привет!', number: 11},
+    {id: 2, text: 'Привет', number: 12},
+    {id: 3, text: 'Как у тебя дела?', number: 9},
+    {id: 4, text: 'Спасибо! Хорошо! Скучаю по тебе!', number: 1},
+    {id: 5, text: "Я тоже очень скучаю по тебе. Рада, что у тебя всё хорошо! Давай увидимся! Ты когда свободен?", number: 4},
+]
+
+
   return (
     <div>
       <div className={classes.item}>
@@ -11,6 +21,12 @@ const My_posts = () => {
         <textarea name="" id="" cols="30" rows="3"></textarea>
         <button>Add post</button>      
       </div>
+
+      <Post message={postsData[0].text} number={postsData[0].number}/>
+      <Post message={postsData[1].text} number={postsData[1].number}/>
+      <Post message={postsData[2].text} number={postsData[2].number}/>
+      <Post message={postsData[3].text} number={postsData[3].number}/>
+      <Post message={postsData[4].text} number={postsData[4].number}/>
       <Post message="Привет! Как дела?" number="5"/>
       <Post message="Спасибо. Отлично!" number="10"/>
       <Post message="Уау!" number="200"/>
