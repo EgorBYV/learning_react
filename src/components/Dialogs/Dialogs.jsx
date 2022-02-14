f44d189f150dcd4c729882e19812d0ce208a144d
EgorBYV <egovrublevskij@gmail.com>
2022-02-14 10:33:44 +0300

    Добавлена texterea, ref, button, onClick, React.createRef в Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,7 @@
 import classes from './Dialogs.module.css'
 import Dialog from './Dialog/Dialog'
 import Message from './Message/Message'
+import React from 'react'
 
 const Dialogs = (props) => {
 
@@ -11,12 +12,23 @@ const Dialogs = (props) => {
     let messagesElements = props.messagesData.map(message => <Message
         text={message.text} />)
 
+    let newMessageElement = React.createRef();    
+
+    let addMessage = () => {
+        let text = newMessageElement.current.value
+        alert(text);
+    }
+
     return (
         <div className={classes.Dialogs}>
             <div className={classes.dialogs_items}>
                 {dialogsElements}
             </div>
             <div className={classes.messages}>
+                <div>
+                    <textarea ref={newMessageElement} cols="30" rows="4"></textarea>
+                    <button onClick={addMessage}>Sent</button>
+                </div>
                 {messagesElements}
             </div>
         </div>
